07f53a35079a5b9e60cb8c6bbf809dda385e25c1
Philip Mendels <p.mendels@gmail.com>
2021-05-24 21:38:38 +0200

    Add travel to other branch

--- test/time-travel.test.ts
+++ test/time-travel.test.ts
@@ -1,6 +1,6 @@
 import { negate } from 'fp-ts-std/Number';
 import { makeRelativeActionConfig } from '../src/helpers';
-import { createInitialHistory } from '../src/internal';
+import { createInitialHistory, getBranchActions } from '../src/internal';
 import {
   makeUndoableState,
   MakeUndoableStateProps,
@@ -31,6 +31,9 @@ const props: MakeUndoableStateProps<State, PBT> = {
       makeActionForUndo: evolve({ payload: negate }),
       updateState: amount => evolve({ count: add(amount) }),
     }),
+  },
+  options: {
+    useBranchingHistory: true,
   },
 };
 
@@ -66,5 +69,114 @@ describe('timeTravel', () => {
     newUState = timeTravel(2);
     expect(newUState.history.currentIndex).toBe(2);
     expect(newUState.state.count).toBe(12);
+  });
+
+  it('works for another branch', () => {
+    const { undoables, timeTravel, undo } = makeUndoableState(props);
+
+    const { addToCount } = undoables;
+
+    newUState = addToCount(2);
+    newUState = addToCount(3);
+    const branch1Id = newUState.history.currentBranchId;
+    undo();
+    newUState = addToCount(5);
+    newUState = addToCount(7);
+    const branch2Id = newUState.history.currentBranchId;
+    undo();
+    newUState = addToCount(11);
+    newUState = addToCount(13);
+    const branch3Id = newUState.history.currentBranchId;
+
+    expect(
+      getBranchActions(newUState.history.branches[branch1Id]).map(
+        action => action.payload
+      )
+    ).toStrictEqual<number[]>([3]);
+
+    expect(
+      getBranchActions(newUState.history.branches[branch2Id]).map(
+        action => action.payload
+      )
+    ).toStrictEqual<number[]>([7]);
+
+    expect(
+      getBranchActions(newUState.history.branches[branch3Id]).map(
+        action => action.payload
+      )
+    ).toStrictEqual<number[]>([2, 5, 11, 13]);
+
+    expect(newUState.history.currentBranchId).toBe(branch3Id);
+    expect(newUState.state.count).toBe(33);
+
+    // use the local index for the current history state: b1 index 0 = payload 3
+    newUState = timeTravel(0, branch1Id);
+
+    expect(
+      getBranchActions(newUState.history.branches[branch1Id]).map(
+        action => action.payload
+      )
+    ).toStrictEqual<number[]>([2, 3]);
+
+    expect(
+      getBranchActions(newUState.history.branches[branch2Id]).map(
+        action => action.payload
+      )
+    ).toStrictEqual<number[]>([7]);
+
+    expect(
+      getBranchActions(newUState.history.branches[branch3Id]).map(
+        action => action.payload
+      )
+    ).toStrictEqual<number[]>([5, 11, 13]);
+
+    expect(newUState.history.currentBranchId).toBe(branch1Id);
+    expect(newUState.state.count).toBe(7);
+
+    newUState = timeTravel(0, branch2Id);
+
+    expect(
+      getBranchActions(newUState.history.branches[branch1Id]).map(
+        action => action.payload
+      )
+    ).toStrictEqual<number[]>([3]);
+
+    expect(
+      getBranchActions(newUState.history.branches[branch2Id]).map(
+        action => action.payload
+      )
+    ).toStrictEqual<number[]>([2, 5, 7]);
+
+    expect(
+      getBranchActions(newUState.history.branches[branch3Id]).map(
+        action => action.payload
+      )
+    ).toStrictEqual<number[]>([11, 13]);
+
+    expect(newUState.history.currentBranchId).toBe(branch2Id);
+    expect(newUState.state.count).toBe(16);
+
+    newUState = timeTravel(1, branch3Id);
+
+    expect(
+      getBranchActions(newUState.history.branches[branch1Id]).map(
+        action => action.payload
+      )
+    ).toStrictEqual<number[]>([3]);
+
+    expect(
+      getBranchActions(newUState.history.branches[branch2Id]).map(
+        action => action.payload
+      )
+    ).toStrictEqual<number[]>([7]);
+
+    expect(
+      getBranchActions(newUState.history.branches[branch3Id]).map(
+        action => action.payload
+      )
+    ).toStrictEqual<number[]>([2, 5, 11, 13]);
+
+    expect(newUState.history.currentBranchId).toBe(branch3Id);
+    expect(newUState.state.count).toBe(33);
   });
 });
